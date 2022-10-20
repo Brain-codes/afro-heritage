7f8a59208206c4aa93434a777c1a09cef816b0e5
adenuga adewumi efe <79708712+Brain-codes@users.noreply.github.com>
2022-10-20 23:55:12 +0100

    Created my design system for the app

---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="cont-1">
+      <button className="button-one">Buy your ticket</button>
+
+      <p className="ts-up">
+        Afro heritage is an ibadan based New International Version Before the
+        mountains were born or you brought forth the whole world, from
+        everlasting to everlasting you are God. This event hosts accomplished
+        women in tech and highlights their success stories within the community,
+        while also sharing tips and takes to growing an outstanding career in
+        tech as a woman. This event hosts accomplished women in tech and
+        highlights their success stories within the community, while also
+        sharing tips and takes to growing an outstanding career in tech as a
+        woman. This event hosts accomplished women in tech and highlights their
+        success stories within the community, while also sharing tips and takes
+        to growing an outstanding career in tech as a woman.This event hosts
+        accomplished women in tech and highlights their success stories within
+        the community, while also sharing tips and takes to growing an
+        outstanding career in tech as a woman.
+      </p>
+      <h2 className="h3-yellow">ABOUT THE EVENT</h2>
+    </div>
+  );
+};
+
+export default Home;
